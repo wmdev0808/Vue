d6f64749743e392240655cecccf5488d8ca4d1e4
Paul <wmdev0808@gmail.com>
2023-01-19 05:11:19 -0800

    -- Create the earlier app using Vue

---
+++ challenge-001/02-rebuilt-app-with-vue/app.js
@@ -0,0 +1,14 @@
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
