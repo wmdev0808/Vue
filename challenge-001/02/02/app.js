8d8ede938e32eed05e17a76cbddaaaf0d9fe5742
Paul <wmdev0808@gmail.com>
2023-01-19 06:26:55 -0800

    -- Define event methods and bind them to click event

--- challenge-001/02/02/app.js
+++ challenge-001/02/02/app.js
@@ -4,6 +4,15 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add() {
+      this.counter = this.counter + 1;
+    },
+    reduce() {
+      this.counter = this.counter - 1;
+      // this.counter--;
+    },
+  },
 });
 
 app.mount("#events");
