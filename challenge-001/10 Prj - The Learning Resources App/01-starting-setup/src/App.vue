eafa98b2e1151dab487c224777d90234bddfc9cb
Paul <wmdev0808@gmail.com>
2023-01-24 07:36:43 -0800

    -- Scaffold the starting setup

---
+++ challenge-001/10 Prj - The Learning Resources App/01-starting-setup/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template></template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
