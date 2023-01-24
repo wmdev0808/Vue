29f529302a9c31566e023683fe26e366a794ab07
Paul <wmdev0808@gmail.com>
2023-01-24 06:31:23 -0800

    -- Teleport elements using `teleport` built-in component

--- challenge-001/09 Cmp Deep Dive/01-starting-setup/src/components/ManageGoals.vue
+++ challenge-001/09 Cmp Deep Dive/01-starting-setup/src/components/ManageGoals.vue
@@ -1,10 +1,41 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
 <script lang="ts">
-export default {};
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = (this.$refs.goal as HTMLInputElement).value;
+
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
 </script>
