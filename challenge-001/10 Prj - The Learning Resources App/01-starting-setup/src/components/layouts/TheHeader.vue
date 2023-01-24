f9dcb5415fdb010848912d698cbea7a3a547738b
Paul <wmdev0808@gmail.com>
2023-01-24 07:39:33 -0800

    -- Create a layout component, TheHeader

---
+++ challenge-001/10 Prj - The Learning Resources App/01-starting-setup/src/components/layouts/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  // props: ["title"],
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #640032;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header h1 {
+  color: white;
+  margin: 0;
+}
+</style>
