ee6af42d67a029ce8363149f10e2cc65cfdbadaa
Paul <wmdev0808@gmail.com>
2023-01-24 07:57:13 -0800

    -- Add a UI component, BaseButton

---
+++ challenge-001/10 Prj - The Learning Resources App/01-starting-setup/src/components/UI/BaseButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from "vue";
+
+export enum ButtonMode {
+  DEFAULT = "DEFAULT",
+  FLAT = "FLAT",
+}
+
+export default defineComponent({
+  // props: ["mode"],
+  props: {
+    mode: {
+      type: String as PropType<ButtonMode>,
+      required: false,
+    },
+  },
+});
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #3a0061;
+  border: 1px solid #3a0061;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #270041;
+  border-color: #270041;
+}
+
+.flat {
+  background-color: transparent;
+  color: #3a0061;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #edd2ff;
+}
+</style>
