7064e690f0c3b179406f56449acc2d8ceae4cb42
Paul <wmdev0808@gmail.com>
2023-01-25 09:28:49 -0800

    -- Add the logics to redirect and catch all routes

--- challenge-001/13 Routing/02-vue-router/src/router/index.ts
+++ challenge-001/13 Routing/02-vue-router/src/router/index.ts
@@ -4,15 +4,17 @@ import TeamsList from "@/views/TeamsList.vue";
 import UsersList from "@/views/UsersList.vue";
 import HomeView from "@/views/HomeView.vue";
 import TeamMembers from "@/views/TeamMembers.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
+    // {
+    //   path: "/",
+    //   name: "home",
+    //   component: HomeView,
+    // },
+    { path: "/", redirect: "/teams" },
     {
       path: "/teams",
       component: TeamsList, // our-domain.com/teams => TeamsList
@@ -34,6 +36,7 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    { path: "/:notFound(.*)*", component: NotFound },
   ],
   linkActiveClass: "active",
 });
