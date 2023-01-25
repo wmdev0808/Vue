34f13eb2a8edb62483cc0e597665033a7741aca4
Paul <wmdev0808@gmail.com>
2023-01-25 01:21:03 -0800

    -- Add v-model for user name field

---
+++ challenge-001/11 Forms/02-forms/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <the-form></the-form>
+</template>
+
+<script setup lang="ts">
+import TheForm from "./components/TheForm.vue";
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+  background-color: #292929;
+}
+</style>
