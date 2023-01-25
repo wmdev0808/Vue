674485122dccded4ddb6eb580b267d9633ba632e
Paul <wmdev0808@gmail.com>
2023-01-25 01:46:07 -0800

    -- Add a basic form validation on userName field

--- challenge-001/11 Forms/02-forms/src/components/TheForm.vue
+++ challenge-001/11 Forms/02-forms/src/components/TheForm.vue
@@ -1,8 +1,18 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: userNameValidity === 'invalid' }"
+    >
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+        @blur="validateInput"
+      />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -115,6 +125,7 @@ export default defineComponent({
       interest: [],
       how: null,
       confirm: false,
+      userNameValidity: "pending",
     };
   },
   methods: {
@@ -138,6 +149,13 @@ export default defineComponent({
       console.log(this.confirm);
       this.confirm = false;
     },
+    validateInput() {
+      if (this.userName === "") {
+        this.userNameValidity = "invalid";
+      } else {
+        this.userNameValidity = "valid";
+      }
+    },
   },
 });
 </script>
@@ -154,6 +172,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
 }
 
 label {
