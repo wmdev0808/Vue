bed974416103d85c2ad96664a182b89a6e5c513e
Paul <wmdev0808@gmail.com>
2023-01-25 05:43:54 -0800

    -- Send a post request to firebase db to save survey results

---
+++ challenge-001/12 Http/02-project/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <learning-survey></learning-survey>
+  <user-experiences></user-experiences>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import type SurveyResult from "./types/SurveyResult";
+import type Rating from "./types/Rating";
+
+export default defineComponent({
+  // data() {
+  //   return {
+  //     savedSurveyResults: [] as SurveyResult[],
+  //   };
+  // },
+  // methods: {
+  //   storeSurvey(surveyData: { userName: string; rating: Rating }) {
+  //     const surveyResult = {
+  //       name: surveyData.userName,
+  //       rating: surveyData.rating,
+  //       id: new Date().toISOString(),
+  //     };
+  //     this.savedSurveyResults.push(surveyResult);
+  //     console.log(surveyResult);
+  //   },
+  // },
+});
+</script>
+<script setup lang="ts">
+import LearningSurvey from "./components/survey/LearningSurvey.vue";
+import UserExperiences from "./components/survey/UserExperiences.vue";
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ challenge-001/12 Http/02-project/src/components/survey/SurveyResult.vue
@@ -0,0 +1,62 @@
+<template>
+  <li>
+    <p>
+      <span class="highlight">{{ name }}</span> rated the learning experience
+      <span :class="ratingClass">{{ rating }}</span
+      >.
+    </p>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from "vue";
+import type Rating from "@/types/Rating";
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: String as PropType<Rating>,
+      required: true,
+    },
+  },
+  computed: {
+    ratingClass(): string {
+      return "highlight rating--" + this.rating;
+    },
+  },
+});
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+
+h3,
+p {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+.highlight {
+  font-weight: bold;
+}
+
+.rating--poor {
+  color: #b80056;
+}
+
+.rating--average {
+  color: #330075;
+}
+
+.rating--great {
+  color: #008327;
+}
+</style>
