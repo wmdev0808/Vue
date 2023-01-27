1c2bcbf403fa5ec25119d36a1b272fe1ea8e6302
Paul <wmdev0808@gmail.com>
2023-01-27 08:20:21 -0800

    -- Add route page components

---
+++ challenge-001/16 Prj - Find a Coach/02-project/src/router/index.ts
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import CoachesList from "@/views/coaches/CoachesList.vue";
+import CoachDetail from "@/views/coaches/CoachDetail.vue";
+import CoachRegistration from "@/views/coaches/CoachRegistration.vue";
+
+import ContactCoach from "@/views/requests/ContactCoach.vue";
+import RequestsReceived from "@/views/requests/RequestsReceived.vue";
+
+import NotFound from "@/views/NotFound.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: "/", redirect: "/coaches" },
+    { path: "/coaches", component: CoachesList },
+    {
+      path: "/coaches/:id",
+      component: CoachDetail,
+      children: [
+        { path: "contact", component: ContactCoach }, // /coaches/c1/contact
+      ],
+    },
+    { path: "/register", component: CoachRegistration },
+    { path: "/requests", component: RequestsReceived },
+    { path: "/:notFound(.*)*", component: NotFound },
+  ],
+});
+
+export default router;
