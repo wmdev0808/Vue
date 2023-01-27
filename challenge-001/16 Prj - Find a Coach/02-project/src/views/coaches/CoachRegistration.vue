96016cde7fcd363be23b36a43141ca4e18ab7672
Paul <wmdev0808@gmail.com>
2023-01-27 10:28:28 -0800

    -- Build a form for registering as a coach

--- challenge-001/16 Prj - Find a Coach/02-project/src/views/coaches/CoachRegistration.vue
+++ challenge-001/16 Prj - Find a Coach/02-project/src/views/coaches/CoachRegistration.vue
@@ -1,1 +1,12 @@
-<template>REGISTER</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script setup lang="ts">
+import CoachForm from "@/components/coaches/CoachForm.vue";
+</script>
