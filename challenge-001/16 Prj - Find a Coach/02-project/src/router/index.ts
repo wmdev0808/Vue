34c2462af6e9df738c48adfdcd5e045679bb6ca6
Paul <wmdev0808@gmail.com>
2023-01-27 10:04:57 -0800

    -- Build CoachDetail component

--- challenge-001/16 Prj - Find a Coach/02-project/src/router/index.ts
+++ challenge-001/16 Prj - Find a Coach/02-project/src/router/index.ts
@@ -17,6 +17,7 @@ const router = createRouter({
     {
       path: "/coaches/:id",
       component: CoachDetail,
+      props: true,
       children: [
         { path: "contact", component: ContactCoach }, // /coaches/c1/contact
       ],
--- challenge-001/16 Prj - Find a Coach/02-project/src/views/coaches/CoachDetail.vue
+++ challenge-001/16 Prj - Find a Coach/02-project/src/views/coaches/CoachDetail.vue
@@ -1,5 +1,69 @@
 <template>
-  DETAILS FOR COACH
-  <router-view></router-view>
-  <router-link to="/coaches/c1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLink">Contact</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script lang="ts">
+import type { Coach } from "@/store/modules/coaches";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      selectedCoach: null as Coach | null,
+    };
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach?.firstName + " " + this.selectedCoach?.lastName;
+    },
+    areas() {
+      return this.selectedCoach?.areas;
+    },
+    rate() {
+      return this.selectedCoach?.hourlyRate;
+    },
+    description() {
+      return this.selectedCoach?.description;
+    },
+    contactLink() {
+      return this.$route.path + "/" + this.id + "/contact";
+    },
+  },
+  created() {
+    this.selectedCoach = (
+      this.$store.getters["coaches/coaches"] as Coach[]
+    ).find((coach) => coach.id === this.id)!;
+  },
+});
+</script>
