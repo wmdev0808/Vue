9a8abd7739dc368f951c5dde986207729cd7d225
Paul <wmdev0808@gmail.com>
2023-01-27 08:25:11 -0800

    -- Add main layout

--- challenge-001/16 Prj - Find a Coach/02-project/src/App.vue
+++ challenge-001/16 Prj - Find a Coach/02-project/src/App.vue
@@ -1,3 +1,24 @@
 <template>
+  <the-header></the-header>
   <router-view></router-view>
 </template>
+
+<script setup lang="ts">
+import TheHeader from "./components/layout/TheHeader.vue";
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
--- challenge-001/16 Prj - Find a Coach/02-project/src/views/coaches/CoachesList.vue
+++ challenge-001/16 Prj - Find a Coach/02-project/src/views/coaches/CoachesList.vue
@@ -1,1 +1,4 @@
-<template></template>
+<template>
+  <section>FILTER</section>
+  <section>LIST OF COACHES</section>
+</template>
