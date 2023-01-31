2f814071c0a36714102fea7ae18f125f3409c963
Paul <wmdev0808@gmail.com>
2023-01-31 04:46:03 -0800

    -- Scaffold the starting setup

---
+++ challenge-001/19 The Composition API/01-starting-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="container">
+    <h2>{{ userName }}</h2>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      userName: "Paul",
+    };
+  },
+});
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
