0aaba06e56f84c67d10b543a1a4c416d759ad1f4
Paul <wmdev0808@gmail.com>
2023-02-01 12:32:32 -0800

    -- Scaffold the composition api starting setup

---
+++ challenge-001/20 Reusing Code/03-composition-api-composables-starting-setup/src/components/AddUser.vue
@@ -0,0 +1,24 @@
+<template>
+  <user-alert v-if="alertIsVisible" title="Add a User?" @close="hideAlert">
+    <p>Do you want to continue with adding a user?</p>
+  </user-alert>
+  <section>
+    <h2>Add a User</h2>
+    <button @click="showAlert">Add User</button>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+import UserAlert from "./UserAlert.vue";
+
+const alertIsVisible = ref<boolean>(false);
+
+function showAlert() {
+  alertIsVisible.value = true;
+}
+function hideAlert() {
+  alertIsVisible.value = false;
+}
+</script>
---
+++ challenge-001/20 Reusing Code/03-composition-api-composables-starting-setup/src/components/DeleteUser.vue
@@ -0,0 +1,23 @@
+<template>
+  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import UserAlert from "./UserAlert.vue";
+
+const alertIsVisible = ref<boolean>(false);
+
+function showAlert() {
+  alertIsVisible.value = true;
+}
+function hideAlert() {
+  alertIsVisible.value = false;
+}
+</script>
