c0235b94a7102954b3be542b0ae0f6b5be28f813
Paul <wmdev0808@gmail.com>
2023-02-02 08:54:13 -0800

    -- Create a simple Vue2 router app with animations

---
+++ challenge-001/22 Vue 2 to 3 Migration/03-router-v2/src/router/index.ts
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import UserList from "@/views/UserList.vue";
+import WelcomeScreen from "@/views/WelcomeScreen.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  base: import.meta.env.BASE_URL,
+  routes: [
+    { path: "/", component: WelcomeScreen },
+    { path: "/users", component: UserList },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
+  ],
+});
+
+export default router;
