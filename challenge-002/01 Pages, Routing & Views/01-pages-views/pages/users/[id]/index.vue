2861e8ecac72579523073ab1f9084d7238a855d1
Paul <wmdev0808@gmail.com>
2023-02-09 06:17:59 -0800

    -- Add `default` layout using `NuxtLayout` and `NuxtPage` -- Get the dynamic route param using `useRoute` composable

---
+++ challenge-002/01 Pages, Routing & Views/01-pages-views/pages/users/[id]/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>A single user, with ID: {{ route.params.id }}</h1>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+</script>
---
+++ challenge-002/01 Pages, Routing & Views/01-pages-views/pages/users/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>The users view</h1>
+</template>
