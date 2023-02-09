e50101adb5f79cecb6c922945a0598caa03a55ab
Paul <wmdev0808@gmail.com>
2023-02-09 13:32:26 -0800

    -- Design homepage

--- challenge-002/01 Pages, Routing & Views/02-project/pages/index.vue
+++ challenge-002/01 Pages, Routing & Views/02-project/pages/index.vue
@@ -1,49 +1,114 @@
 <template>
-  <section class="container">
-    <div>
-      <Logo />
-      <h1 class="title">nuxt-course</h1>
-      <h2 class="subtitle">Nuxt.js course</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
-    </div>
-  </section>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <NuxtLink :to="'/posts/' + 1" class="post-preview">
+        <article>
+          <div
+            class="post-thumbnail"
+            style="
+              background-image: url('https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg');
+            "
+          ></div>
+          <div class="post-content">
+            <h1>Post Title</h1>
+            <p>Preview Text</p>
+          </div>
+        </article>
+      </NuxtLink>
+      <NuxtLink :to="'/posts/' + 2" class="post-preview">
+        <article>
+          <div
+            class="post-thumbnail"
+            style="
+              background-image: url('https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg');
+            "
+          ></div>
+          <div class="post-content">
+            <h1>Post Title 2</h1>
+            <p>Preview Text 2</p>
+          </div>
+        </article>
+      </NuxtLink>
+    </section>
+  </div>
 </template>
 
-<style>
-.container {
-  min-height: 100vh;
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
   display: flex;
-  justify-content: center;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
   align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
   text-align: center;
 }
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links {
-  padding-top: 15px;
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
 }
 </style>
