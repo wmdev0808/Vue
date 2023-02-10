0449d0386c17ba1c820593cd132b6cd3218c0f37
Paul <wmdev0808@gmail.com>
2023-02-10 00:50:32 -0800

    -- Create `PostsPostPreview` component

---
+++ challenge-002/01 Pages, Routing & Views/02-project/components/posts/PostsPostPreview.vue
@@ -0,0 +1,73 @@
+<template>
+  <NuxtLink :to="'/posts/' + id" class="post-preview">
+    <article>
+      <div
+        class="post-thumbnail"
+        :style="{ backgroundImage: 'url(' + thumbnail + ')' }"
+      ></div>
+      <div class="post-content">
+        <h1>{{ title }}</h1>
+        <p>{{ previewText }}</p>
+      </div>
+    </article>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  previewText: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
--- challenge-002/01 Pages, Routing & Views/02-project/pages/index.vue
+++ challenge-002/01 Pages, Routing & Views/02-project/pages/index.vue
@@ -4,34 +4,24 @@
       <h1>Get the latest tech news!</h1>
     </section>
     <section class="featured-posts">
-      <NuxtLink :to="'/posts/' + 1" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            style="
-              background-image: url('https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg');
-            "
-          ></div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </NuxtLink>
-      <NuxtLink :to="'/posts/' + 2" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            style="
-              background-image: url('https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg');
-            "
-          ></div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview Text 2</p>
-          </div>
-        </article>
-      </NuxtLink>
+      <PostsPostPreview
+        id="1"
+        thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        title="Hello there!"
+        previewText="This my first post!"
+      />
+      <PostsPostPreview
+        id="2"
+        thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        title="Hello there - the second time!"
+        previewText="This my second post!"
+      />
+      <PostsPostPreview
+        id="3"
+        thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        title="Hi!"
+        previewText="This my third post!"
+      />
     </section>
   </div>
 </template>
@@ -74,41 +64,5 @@
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
