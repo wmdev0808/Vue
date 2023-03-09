892c4781e8ee867d0880eff1997918619919eb56
Paul <wmdev0808@gmail.com>
2023-03-09 09:24:17 -0800

    -- Handle errors

---
+++ challenge-002/06 The Server Side/01-server-directory/server/api/validation/[id].ts
@@ -0,0 +1,11 @@
+// Error handling
+export default defineEventHandler((event) => {
+  const id = parseInt(event.context.params!.id) as number;
+  if (!Number.isInteger(id)) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "ID should be an integer",
+    });
+  }
+  return "All good";
+});
