0189b21b05846e20feefd8a1ecdee3a3de6c9531
Paul <wmdev0808@gmail.com>
2023-04-06 07:54:10 -0700

    -- Add Vue2 test cases

---
+++ challenge-003/vue2/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import Vue2 from "@vitejs/plugin-vue2";
+
+export default defineConfig({
+  plugins: [Vue2()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    alias: [{ find: /^vue$/, replacement: "vue/dist/vue.runtime.common.js" }],
+  },
+});
