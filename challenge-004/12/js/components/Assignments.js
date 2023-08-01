03bea12270f0936fd98ac4b9e9dabda4f7d9f575
Paul <wmdev0808@gmail.com>
2023-08-01 23:13:42 +0800

    -- Implement v-model within `AssignmentTags` component

---
+++ challenge-004/12/js/components/Assignments.js
@@ -0,0 +1,46 @@
+import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
+
+export default {
+  components: { AssignmentList, AssignmentCreate },
+
+  template: `
+        <section class="space-y-6">
+            <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
+            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+            
+            <assignment-create @add="add"></assignment-create> 
+        </section>
+    `,
+
+  data() {
+    return {
+      assignments: [
+        { name: "Finish project", complete: false, id: 1, tag: "math" },
+        { name: "Read Chapter 4", complete: false, id: 2, tag: "science" },
+        { name: "Turn in Homework", complete: false, id: 3, tag: "math" },
+      ],
+    };
+  },
+
+  computed: {
+    filters() {
+      return {
+        inProgress: this.assignments.filter(
+          (assignment) => !assignment.complete
+        ),
+        completed: this.assignments.filter((assignment) => assignment.complete),
+      };
+    },
+  },
+
+  methods: {
+    add(name) {
+      this.assignments.push({
+        name: name,
+        completed: false,
+        id: this.assignments.length + 1,
+      });
+    },
+  },
+};
